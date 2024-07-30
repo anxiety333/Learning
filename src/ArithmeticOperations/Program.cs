80baa480ed5a28a10e92c85aba70fe435f0018c8
anxiety333 <rudolph.munirov.deals@gmail.com>
2024-07-31 00:14:56 +0500

    second commit

---
+++ src/ArithmeticOperations/Program.cs
@@ -0,0 +1,65 @@
+﻿void BinaryAO()
+{
+    /* + */
+    {
+        int a = 10;
+        int b = a + 12; // 22;
+    }
+    /* - */
+    {
+        int a = 10;
+        int b = a - 6; // 4;
+    }
+    /* * */
+    {
+        int a = 10;
+        int b = a * 5; // 50;
+    }
+    /* / */
+    {
+        int a = 10;
+        int b = a / 5; // 2
+
+        double c = 10;
+        double d = 3;
+        double e = c / d; // 3.33333333
+    }
+    {
+        double a = 10.0 / 4.0; // 2.5
+        double b = 10.0;
+        double c = b % 4.0; //2
+    }
+}
+void UnaryAO()
+{
+    /* ++ */
+    {
+        int x1 = 7;
+        int b1 = ++x1; //b1=8 x1=8
+        Console.WriteLine($"{x1} - {b1}");
+    }
+    {
+        int x1 = 7;
+        int b1 = x1++; // b1=8 x1=7
+        Console.WriteLine($"{x1} - {b1}");
+    }
+    /* -- */
+    {
+        int x1 = 7;
+        int b1 = --x1; //b1=6 x1=6
+        Console.WriteLine($"{x1} - {b1}");
+    }
+    {
+        int x1 = 7;
+        int b1 = x1--; // b1=6 x1=7
+        Console.WriteLine($"{x1} - {b1}");
+    }
+    /* priority*/
+    {
+        int a = 3;
+        int b = 5;
+        int c = 40;
+        int d = c-- - b * a;    // d = (c--)-(b*a);
+
+    }
+}
---
+++ src/ConsoleIO/Program.cs
@@ -0,0 +1,55 @@
+﻿void ConsoleInput()
+{
+    {
+        string hello = "Привет мир";
+        Console.WriteLine(hello);
+        Console.WriteLine("Добро пожаловать в C#!");
+        Console.WriteLine("Пока мир...");
+        Console.WriteLine(24.5);
+    }
+    {
+        string name = "Tom";
+        byte age = 34;
+        float height = 1.7f;
+        Console.WriteLine($"Имя: {name}  Возраст: {age}  Рост: {height}м");
+    }
+    {
+        string name = "Tom";
+        byte age = 34;
+        float height = 1.7f;
+        Console.WriteLine("Имя: {0}  Возраст: {2}  Рост: {1}м", name, height, age);
+    }
+
+}
+void ConsoleWrite()
+{
+    {
+        string name = "Tom";
+        int age = 34;
+        double height = 1.7;
+        Console.Write($"Имя: {name}  Возраст: {age}  Рост: {height}м");
+    }
+}
+void ConsoleInp()
+{
+    {
+        Console.Write("Введите свое имя: ");
+        string? name = Console.ReadLine();
+        Console.WriteLine($"Привет {name}");
+    }
+    {
+        Console.Write("Введите имя: ");
+        string? name = Console.ReadLine();
+
+        Console.Write("Введите возраст: ");
+        byte age = Convert.ToByte(Console.ReadLine());
+
+        Console.Write("Введите рост: ");
+        double height = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите размер зарплаты: ");
+        decimal salary = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine($"Имя: {name}  Возраст: {age}  Рост: {height}м  Зарплата: {salary}$");
+    }
+}
