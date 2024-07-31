d09c5e26caacf19f889ce92dba2f343dfaae25f3
anxiety333 <rudolph.munirov.deals@gmail.com>
2024-07-31 23:16:32 +0500

    upd4 31.07.2024

---
+++ src/ConditionalExpressions/Program.cs
@@ -0,0 +1,70 @@
+﻿void ComparisonOperations()
+{
+    /* == */
+    {
+        int a = 10;
+        int b = 5;
+        bool c = a == b; //false
+    }
+    /* != */
+
+    {
+        int a = 10;
+        int b = 4;
+        bool c = a != b; // true
+        bool d = a != 10; // false
+    }
+    /* < */
+    {
+        int a = 10;
+        int b = 4;
+        bool c = a < b; //false
+    }
+    /* > */
+    {
+        int a = 10;
+        int b = 4;
+        bool c = a > b; // true
+        bool d = a > 25; // false
+    }
+    /* <= */
+    {
+        int a = 10;
+        int b = 4;
+        bool c = a >= b; // true
+        bool d = a >= 25; // false
+    } 
+}
+void LogicalOperations()
+{
+    /* | */
+    {
+        bool x1 = (5 > 6) | (4 < 6); // 5 > 6 - false, 4 < 6 - true, поэтому возвращается true
+        bool x2 = (5 > 6) | (4 > 6); // 5 > 6 - false, 4 > 6 - false, поэтому возвращается false
+    }
+    /* & */
+    {
+        bool x1 = (5 > 6) & (4 < 6); // 5 > 6 - false, 4 < 6 - true, поэтому возвращается false
+        bool x2 = (5 < 6) & (4 < 6); // 5 < 6 - true, 4 < 6 - true, поэтому возвращается true
+    }
+    /* || */
+    {
+        bool x1 = (5 > 6) || (4 < 6); // 5 > 6 - false, 4 < 6 - true, поэтому возвращается true
+        bool x2 = (5 > 6) || (4 > 6); // 5 > 6 - false, 4 > 6 - false, поэтому возвращается false
+    }
+    /* && */
+    {
+        bool x1 = (5 > 6) && (4 < 6); // 5 > 6 - false, 4 < 6 - true, поэтому возвращается false
+        bool x2 = (5 < 6) && (4 < 6); // 5 < 6 - true, 4 < 6 - true, поэтому возвращается true
+    }
+    /* ! */
+    {
+        bool a = true;
+        bool b = !a; //false
+    }
+    /* ^ */
+    {
+        bool x5 = (5 > 6) ^ (4 < 6); // 5 > 6 - false, 4 < 6 - true, поэтому возвращается true
+        bool x6 = (50 > 6) ^ (4 / 2 < 3); // 50 > 6 - true, 4/2 < 3 - true, поэтому возвращается false
+    }
+    } 
