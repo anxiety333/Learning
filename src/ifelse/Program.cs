f2675b7ffd678f00c97e583f3238debea9cc0640
anxiety333 <rudolph.munirov.deals@gmail.com>
2024-08-04 22:46:58 +0500

    upd 04.08.2024

---
+++ src/ifelse/Program.cs
@@ -0,0 +1,42 @@
+﻿void iftest()
+{
+    {
+        int num1 = 8;
+        int num2 = 6;
+        if (num1 > num2)
+        {
+            Console.WriteLine($"Число {num1} больше числа {num2}");
+        }
+    }
+    {
+        int num1 = 8;
+        int num2 = 6;
+        if (num1 > num2 && num1 == 8) Console.WriteLine($"Число {num1} больше числа {num2}");
+    }
+}
+void elsetest()
+{
+    {
+        int num1 = 8;
+        int num2 = 6;
+        if (num1 > num2) Console.WriteLine($"Число {num1} больше числа {num2}");
+        else Console.WriteLine($"Число {num2} больше числа {num1}");
+    }
+}
+void elseiftest()
+{
+    {
+        int num1 = 8;
+        int num2 = 6;
+        if (num1 > num2) Console.WriteLine($"Число {num1} больше числа {num2}");
+        else if (num1 < num2) Console.WriteLine($"Число {num2} больше числа {num1}");
+        else Console.WriteLine($"Число {num1} равно числу {num2}");
+    }
+}
+void TernaryOperation()
+{
+    int x = 3;
+    int y = 2;
+    int z = x < y ? (x + y) : (x - y);
+    Console.WriteLine(z);
+}
---
+++ src/ifelse/ifelsepractice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifelse
+{
+    internal class ifelsepractice
+    {
+        
+        /* Напишите консольную программу, в которую пользователь вводит с клавиатуры два числа. А программа
+         * сранивает два введенных числа и выводит на консоль результат сравнения 
+         * (два числа равны, первое число больше второго или первое число меньше второго).*/
+        void CompareNumbers()
+        {
+            Console.WriteLine("Введите два числа:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2) Console.WriteLine($"Число {num1} больше числа {num2}");
+            else if (num1 < num2) Console.WriteLine($"Число {num1} меньше числа {num2}");
+            else Console.WriteLine($"Числа {num1} и {num2} равны");    
+        }
+
+        void MoreLessNumbers()
+        {
+            Console.WriteLine($"Введите два числа:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > 5 && num1 < 10) Console.WriteLine($"Число больше 5 и меньше 10");
+            else Console.WriteLine("Неизвестное число");
+        }
+    }
+}
