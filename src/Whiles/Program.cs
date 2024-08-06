73ce5fb04e37c92c5fcbe88f92d865eac8185e27
anxiety333 <rudolph.munirov.deals@gmail.com>
2024-08-06 20:28:09 +0500

    upd 06.08.24

---
+++ src/Whiles/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Runtime.ExceptionServices;
+
+void Whiles()
+{
+    {
+        for (int i = 1; i < 4; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+    { for (int i = 1; i < 4; i++) Console.WriteLine(i); }
+    {
+        int i = 1;
+
+        for (Console.WriteLine("Начало выполнения итерации"); i < 4; Console.WriteLine($"i = {i}"))
+        {
+            i++;
+        }
+    }
+    {
+        int i = 1;
+        for (; i<4 ; )
+        {
+            Console.WriteLine($"i={i}");
+            i++;
+        }
+    }
+    {
+        for (int i = 1, j = 2; i < 10; i++, j++)
+        {
+            Console.WriteLine($"{i*j} \t");
+        }
+        Console.WriteLine();
+    }
+}
+void DoWhiles()
+{
+    {
+        int i = 6;
+        do
+        {
+            Console.WriteLine(i);
+            i--;
+        }
+        while (i > 0);
+    }
+    {
+        int i = 6;
+        while (i > 0)
+        {
+            Console.WriteLine(i);
+            i--;
+        }
+    }
+    {
+        foreach (char c in "TOM")
+        {
+            Console.WriteLine(c);
+        }
+    }
+    {
+        for (int i = 1; i < 10; i++)
+        {
+            for (int j = 1; j < 10; j++)
+            {
+                Console.Write($"{i * j} \t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ src/Whiles/WhilesPractice.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whiles
+{
+    internal class WhilesPractice
+    {
+        /*За каждый месяц банк начисляет к сумме вклада 7% от суммы. Напишите консольную программу, 
+         * в которую пользователь вводит сумму вклада и количество месяцев. А банк вычисляет конечную сумму вклада с учетом начисления 
+         * процентов за каждый месяц.Для вычисления суммы с учетом процентов используйте цикл for. 
+         * Для ввода суммы вклада используйте выражение Convert.ToDecimal(Console.ReadLine()) (сумма вклада будет представлять тип decimal).*/
+
+        void DepositCalculate()
+        {
+            Console.WriteLine("Введите сумму вклада и количество месяцев");
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+            int mounth = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < mounth; i++)
+            {
+                sum += sum * 0.07M;
+                Console.WriteLine(sum);
+            }
+        }
+        /*Перепишите предыдущую программу, только вместо цикла for используйте цикл while.*/
+        void DepositCalculateWhile()
+        {
+            int i = 1;
+            Console.WriteLine("Введите сумму вклада и количество месяцев");
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+            int mounth = Convert.ToInt32(Console.ReadLine());
+            while (i<mounth)
+            {
+                sum += sum * 0.07M;
+                Console.WriteLine(sum);
+                i++;
+            }
+        }
+    }
+}
