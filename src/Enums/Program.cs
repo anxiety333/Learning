be2445c16bc65cb6e9c97432ce1b73c0ded15b3f
anxiety333 <rudolph.munirov.deals@gmail.com>
2024-08-19 22:22:00 +0500

    upd 19.08.24

---
+++ src/Enums/Program.cs
@@ -0,0 +1,14 @@
+﻿DayTime dayTime = DayTime.Morning;
+
+if (dayTime == DayTime.Morning)
+    Console.WriteLine("Доброе утро");
+else
+    Console.WriteLine("Привет");
+
+enum DayTime
+{
+    Morning,
+    Afternoon,
+    Evening,
+    Night
+}
---
+++ src/SwitchCase/Program.cs
@@ -0,0 +1,82 @@
+﻿void SwitchCase()
+{
+    {
+        string name = "Bob";
+
+        switch (name)
+        {
+            case "Bob":
+                Console.WriteLine("Ваше имя Bob");
+                break;
+            case "Tom":
+                Console.WriteLine("Ваше имя Tom");
+                break;
+            case "Sam":
+                Console.WriteLine("Ваше имя Sam");
+                break;
+        }
+
+    }
+    {
+        string name = "Bob";
+
+        switch (name)
+        {
+            case "Bob":
+                Console.WriteLine("Ваше имя Bob");
+                break;
+            case "Tom":
+                Console.WriteLine("Ваше имя Tom");
+                break;
+            case "Sam":
+                Console.WriteLine("Ваше имя Sam");
+                break;
+            default:
+                Console.WriteLine("А я хз кто ты");
+                break;
+        }
+
+    }
+    {
+        int number = 1;
+
+        switch (number)
+        {
+            case 1:
+                Console.WriteLine("case 1");
+                goto case 5;
+            case 2:
+                Console.WriteLine("case 2");
+                break;
+            case 5:
+                Console.WriteLine("case 5");
+                break;
+            default:
+                Console.WriteLine("default");
+                break;
+        }
+
+    }
+    int DoOperation(int op, int a, int b)
+    {
+        switch (op)
+        {
+            case 1: return a + b;
+            case 2: return a - b;
+            case 3: return a * b;
+            default: return 0;
+        }
+    }
+    /*Получение результата из switch*/
+    int DoOperation1(int op, int a, int b)
+    {
+        int result = op switch
+        {
+            1 => a + b,
+            2 => a - b,
+            3 => a * b,
+            _=>0
+        };
+        return result;
+    }
+}
